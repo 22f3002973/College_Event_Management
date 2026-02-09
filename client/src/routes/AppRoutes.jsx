bd901aba049c9501e601a1d40eb293d9424c72b1
padmasee05 <padmasee0511@gmail.com>
2026-02-10 02:00:28 +0530

    added stud feedback route

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -7,6 +7,7 @@ import Login from "../pages/Authentication/Login";
 import StudentDashboard from "../pages/Student/StudentDashboard";
 import BrowseEvents from "../pages/Student/BrowseEvents";
 import RegisteredEvents from "../pages/Student/RegisteredEvents";
+import StudentFeedback from "../pages/Student/StudentFeedback";
 
 // Organizer
 import OrganizerDashboard from "../pages/Organizer/OrganizerDashboard";
@@ -26,6 +27,7 @@ const AppRoutes = () => {
       <Route path="/student/dashboard" element={<StudentDashboard />} />
       <Route path="/student/browse" element={<BrowseEvents />} />
       <Route path="/student/registered" element={<RegisteredEvents />} />
+      <Route path="/student/feedback" element={<StudentFeedback />} /> 
 
       <Route path="/organizer/dashboard" element={<OrganizerDashboard />} />
       <Route path="/organizer/create-event" element={<CreateEvent />} />
