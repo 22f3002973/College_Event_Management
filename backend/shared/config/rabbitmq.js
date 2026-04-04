3e075c51543d9d84fa74d7e0ce99641a4093a12e
Sakshi101214 <Sakshikumari29353@gmail.com>
2026-04-04 12:19:32 +0530

    latest changes

--- backend/shared/config/rabbitmq.js
+++ backend/shared/config/rabbitmq.js
@@ -5,15 +5,11 @@ let channel;
 async function connectQueue() {
   const connection = await amqp.connect("amqp://localhost");
   channel = await connection.createChannel();
-
   await channel.assertQueue("eventQueue");
-  console.log("RabbitMQ Connected");
 }
 
 function sendMessage(msg) {
-  if (channel) {
-    channel.sendToQueue("eventQueue", Buffer.from(msg));
-  }
+  channel.sendToQueue("eventQueue", Buffer.from(JSON.stringify(msg)));
 }
 
 module.exports = { connectQueue, sendMessage };
