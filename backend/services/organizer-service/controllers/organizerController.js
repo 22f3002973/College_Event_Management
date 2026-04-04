b41b3ed55fbf0422ae7bd667a777ff0c2d8e8fe0
Sakshi101214 <Sakshikumari29353@gmail.com>
2026-04-04 12:23:02 +0530

    added organizer service folder

---
+++ backend/services/organizer-service/controllers/organizerController.js
@@ -0,0 +1,16 @@
+
+// const { sendMessage } = require("../../shared/config/rabbitmq");
+
+// exports.registerUser = async (req, res) => {
+//   const user = req.body;
+
+//   // Save to DB (assume done)
+
+  
+//   sendMessage({
+//     type: "USER_REGISTERED",
+//     data: user,
+//   });
+
+//   res.json({ message: "User registered" });
+// };
