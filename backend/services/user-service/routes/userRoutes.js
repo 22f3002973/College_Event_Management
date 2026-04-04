59b27ca8ba61c379bc1759fcdf42d7ba91515027
Sakshi101214 <Sakshikumari29353@gmail.com>
2026-04-04 12:53:35 +0530

    Added user service and backend structure

---
+++ backend/services/user-service/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+
+    res.json({ message: "User saved" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
